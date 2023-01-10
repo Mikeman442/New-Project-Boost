206f967172872914010c248935b63fb964bf1ddb
Michael Monteleone <Michael.w.conway@gmail.com>
2023-01-09 20:26:11 -0500

    Finished Oscilation script. created prefab moving obstacles with oscilation script.

--- Assets/Scripts/Oscilator.cs
+++ Assets/Scripts/Oscilator.cs
@@ -8,6 +8,7 @@ public class Oscilator : MonoBehaviour
     Vector3 startingPosition;
     [SerializeField] Vector3 movementVector;
     [SerializeField] [Range(0,1)] float movementFactor;
+    [SerializeField] float period;
 
     // Start is called before the first frame update
     void Start()
@@ -19,6 +20,13 @@ public class Oscilator : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        float cycles = Time.time / period;
+
+        const float tau = Mathf.PI * 2;
+        float rawSinWave = Mathf.Sin(cycles * tau);
+
+        movementFactor = (rawSinWave + 1f) / 2f;
+
         Vector3 offset = movementVector * movementFactor;
         transform.position = startingPosition + offset;
     }
